4ccbc605416a20dad0a755265c59a7d380a9e5a1
Arthur <Arthur.fabricyo@gmail.com>
2025-08-04 00:01:15 -0300

    Configuração básica de todo o app finalizado

---
+++ src/screens/index.tsx
@@ -0,0 +1,11 @@
+
+import { Text, View } from 'react-native'
+
+export function Login() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+        <Text>Bem vindo a tela de login</Text>
+    </View>
+  )
+}
+
