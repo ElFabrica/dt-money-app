1709194b73982b2205b3ecb1d8febaa962ec4082
Arthur <Arthur.fabricyo@gmail.com>
2025-08-21 14:19:20 -0300

    Identação corrijida

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -2,49 +2,49 @@ import { FormLoginParams } from "@/screens/login/LoginForm"
 import { FormRegisterParams } from "@/screens/Register/RegisterForm"
 import { createContext, FC, PropsWithChildren, useContext, useState } from "react"
 
-type AuthContextType ={
+type AuthContextType = {
     user: null
     token: string | null
-    handleAuthenticate: ( params: FormLoginParams) => Promise<void>
+    handleAuthenticate: (params: FormLoginParams) => Promise<void>
     handleRegister: (params: FormRegisterParams) => Promise<void>
     handleLogout: () => void
 }
 
-export const AuthContext = createContext<AuthContextType | null>(
+export const AuthContext = createContext<AuthContextType>(
     {} as AuthContextType)
 
- export const AuthContextProvider: FC<PropsWithChildren > = ({ children }) => {
-    const [user, setUser] = useState (null)
+export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
+    const [user, setUser] = useState(null)
     const [token, serToken] = useState<string | null>(null)
-    
-    const handleAuthenticate = async ({email, password}:FormLoginParams) =>{
+
+    const handleAuthenticate = async ({ email, password }: FormLoginParams) => {
 
     }
 
     const handleRegister = async (formData: FormRegisterParams) => {
 
     }
-    const handleLogout = () =>{
+    const handleLogout = () => {
 
     }
 
-    return(
+    return (
         <AuthContext.Provider
-        value={{
-            handleAuthenticate, 
-            handleRegister, 
-            handleLogout, 
-            token, 
-            user,
-        }}
+            value={{
+                handleAuthenticate,
+                handleRegister,
+                handleLogout,
+                token,
+                user,
+            }}
         >
             {children}
         </AuthContext.Provider>
     );
- }
+}
 
- export const useAuthContext = () => {
+export const useAuthContext = () => {
     const context = useContext(AuthContext)
 
     return context
- }
+}
